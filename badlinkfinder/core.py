06dd8ceba231e5aeb32230e842e0d3e8618306f5
Stan Brinkerhoff <stan.brinkerhoff@coxautoinc.com>
2018-04-29 19:18:10 -0400

    Initial breakout of  into smaller component, with  becoming the stateful component

--- badlinkfinder/core.py
+++ badlinkfinder/core.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-from badlinkfinder import __version__ as blf_version
+import logging
+
 from badlinkfinder.cli import parser, extra_parse_logic
 from badlinkfinder.crawler import Crawler
+from badlinkfinder.engine import Engine
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 def main():
     args = extra_parse_logic(parser.parse_args())
 
+    if args.verbosity == 4:
+        logger.setLevel(logging.DEBUG)
+
     try:
         crawler = Crawler(args)
-        errors = crawler.run(args.url)
+        engine = Engine(crawler=crawler, url=args.url)
+
+        engine.main()
+
+        errors = engine.get_errors()
+
+        #errors = engine.crawler.run(args.url)
+
     except KeyboardInterrupt:
         # Catch Interrupt and save progress in case you want to restart it. (TODO)
         sys.exit('\nERROR: Interrupted by user')
@@ -29,3 +44,6 @@ def main():
             print('\nHere are the issues that were found:')
             for error in errors:
                 print(error)
+
+if __name__ == "__main__":
+    main()
